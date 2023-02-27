9a6a2f70e830e11dae9dbc66f3f491be1f89396e
Alexandre <alexandrefsoficial@gmail.com>
2023-02-27 15:57:23 -0300

    get url by id implemented

--- src/controller/URL.js
+++ src/controller/URL.js
@@ -6,7 +6,7 @@ export async function postUrl(req, res) {
     const token = authorization.replace('Bearer ', '');
     const { url } = req.body;
     try {
-        if(!token){
+        if (!token) {
             return res.sendStatus(401);
         }
         const userExists = await db.query(`
@@ -22,12 +22,28 @@ export async function postUrl(req, res) {
             INSERT INTO "ShortenedUrls" 
             (url,"visitCount","shortUrl","userId","createdAt")
             VALUES ($1,0,$2,$3,NOW())
-        `,[url,shortUrl,userId]);
+        `, [url, shortUrl, userId]);
         const urlTable = await db.query(`
             SELECT id, "shortUrl" FROM "ShortenedUrls"
             WHERE "shortUrl" = $1   
-        `,[shortUrl]);
-        return res.send(urlTable.rows[0])
+        `, [shortUrl]);
+        return res.send(urlTable.rows[0]);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
+
+export async function getUrlById(req, res) {
+    try {
+        const {id} = req.params
+        const query = await db.query(`
+            SELECT id,"shortUrl",url FROM "ShortenedUrls"
+            Where id=$1
+        `,[id])
+        if(query.rows.length === 0){
+            return res.sendStatus(404)
+        }
+        return res.status(200).send(query.rows[0])
     } catch (err) {
         return res.status(500).send(err.message);
     }
--- src/routes/URLRouter.js
+++ src/routes/URLRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { validateSchema } from '../middleware/validateSchema.js';
-import { postUrl } from '../controller/URL.js';
+import { postUrl, getUrlById } from '../controller/URL.js';
 import { PostUrlSchema } from '../schema/URLSchema.js';
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", validateSchema(PostUrlSchema), postUrl);
+urlRouter.get("/urls/:id", getUrlById)
 
 export default urlRouter;
